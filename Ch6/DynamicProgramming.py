c83e9b47b0f3b355c1e696ab7d26ec05ee25c084
forcer0625 <elison8934678@gmail.com>
2023-10-17 12:29:47 +0800

    6-1 done

---
+++ Ch6/DynamicProgramming.py
@@ -0,0 +1,101 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from copy import deepcopy
+from Games import NormalFormGame, StochasticGame
+
+def mean_square_error(a, b):
+        return np.square(np.subtract(a, b)).mean()
+
+class ValueIteration:
+    def __init__(self, env: StochasticGame, gamma=0.99, max_steps=None):
+        self.env = env
+        self.gamma = gamma
+        self.converge_bound = .05
+        self.value = np.zeros(shape=(env.n_agents, env.n_states))
+        self.max_steps = max_steps
+
+    def value_iteration(self):
+        # 1. Initialize Vi(s)
+        self.value = np.zeros(shape=(env.n_agents, env.n_states))
+        matrix = np.zeros(shape=(self.env.n_agents, self.env.n_states, self.env.n_joint_actions))
+        record = []
+        for i in range(self.env.n_agents):
+            record.append([])
+        if self.max_steps is None:
+            prev_value = deepcopy(self.value)
+        step = 0
+        flag = True
+
+        # 2. Repeat 3.~4. until converged or reach max steps
+        while flag:
+            # 3. Compute matrix
+            for i in range(self.env.n_agents):
+                for s in range(self.env.n_states):
+                    for a in range(self.env.n_joint_actions):
+                        self.compute_matrix(matrix, i ,s ,a)
+
+            # 4. Update Vi(s)
+            for i in range(self.env.n_agents):
+                for s in range(self.env.n_states):
+                    self.update(matrix, i, s)
+                mse = mean_square_error(prev_value, self.value)
+                record[i].append(mse)
+                if mse <= self.converge_bound-0.04995:
+                    flag = False
+                elif self.max_steps is not None and step >= self.max_steps:
+                    flag = False
+            prev_value = deepcopy(self.value)
+
+        return record
+
+    def update(self, mat, agent, state):
+        self.value[agent, state] = np.max(mat[agent, state])
+
+    def compute_matrix(self, mat, agent, state, joint_action):
+        mat[agent, state, joint_action] = 0
+        for next_s in range(self.env.n_states):
+            mat[agent, state, joint_action]+=\
+                self.env.state_transition_matrix[state, joint_action, next_s]*\
+                (self.env.state_matrixs[state, joint_action, agent]+self.gamma*self.value[agent, next_s])
+
+    def non_repeated_normal_from_game_minimax(self):
+        pass
+
+if __name__ == '__main__':
+    # 1. Create stochastic game as an environment
+    s1 = np.array(
+        [   [1, -1],[0, 0],
+            [2, -2],[-1, 1]]
+    )
+    s2 = np.array(
+        [   [3, -3],[0, 0],
+            [-2, 2],[-3, 3]]
+    )
+    s3 = np.array(
+        [   [0, 0],[0, 0],
+            [1, -1],[-1, 1]]
+    )
+    state_transition_matrix = np.array([
+            [[1.0, 0.0, 0.0], [1.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.5, 0.5, 0.0]],
+            [[0.0, 1.0, 0.0], [0.8, 0.0, 0.2], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]],
+            [[0.0, 0.0, 1.0], [0.0, 0.0, 1.0], [1.0, 0.0, 0.0], [0.0, 0.0, 1.0]]
+        ], ndmin=3)
+    env = StochasticGame(
+        n_agents=2,
+        n_states=3,
+        n_actions=2,
+        state_matrixs=np.array([s1, s2, s3]),
+        state_transition_matrix=state_transition_matrix,
+    )
+    algo = ValueIteration(env=env, gamma=0.99)
+    record = algo.value_iteration()
+    #for i in range(env.n_agents):
+    plt.plot(record[0], color='aqua')
+    plt.plot(record[1], color='blue')
+    plt.plot([0.05 for i in range(max(len(record[0]),len(record[1])))], 'r:')
+    plt.xlabel('Steps')
+    plt.ylabel('Mean Square Error')
+    plt.legend(['Agent 1', 'Agent 2', 'Convergence Boundary:{value}'.format(value=algo.converge_bound)], loc='best')
+    print(algo.value)
+    plt.show()
+    
---
+++ Ch6/Games.py
@@ -0,0 +1,120 @@
+import numpy as np
+import random
+
+class NormalFormGame:
+    def __init__(self, n_agents:int, n_actions:tuple|int, state_matrix) -> None:
+        self.n_agents = n_agents
+        if type(n_actions)==tuple:
+            self.n_actions = n_actions
+        else:
+            self.n_actions = tuple(n_actions for i in range(n_agents))
+        self.n_joint_actions = 1
+        for actions in self.n_actions:
+            self.n_joint_actions*=actions
+        self.state_matrix = np.reshape(state_matrix, (-1, n_agents))
+
+    def flatten_joint_action(self, joint_action):
+        if type(joint_action) is int:
+            return joint_action
+        index = 0
+        for i in range(self.n_agents):
+            index += (joint_action[i]*pow(self.n_actions[0],self.n_agents-i-1))
+        return index
+    
+    def step(self, joint_action:np.ndarray|tuple):
+        return self.joint_reward(joint_action)
+    
+    def joint_reward(self, joint_action:np.ndarray|tuple):
+        return self.state_matrix[self.flatten_joint_action(joint_action)]
+    
+    def reward(self, joint_action:np.ndarray|tuple, agent=None):
+        if agent is None:
+            return self.joint_reward(joint_action)
+        return self.joint_reward(joint_action)[agent]
+    
+    
+
+class StochasticGame(NormalFormGame):
+    def __init__(self,
+                 n_agents:int,
+                 n_states:int,
+                 n_actions:tuple|int,
+                 state_matrixs,
+                 state_transition_matrix,
+                 initial_state=None,
+                 terminal_state=None,
+                 ) -> None:
+        self.n_agents = n_agents
+        self.n_states = n_states
+        if type(n_actions)==tuple:
+            self.n_actions = n_actions
+        else:
+            self.n_actions = tuple(n_actions for i in range(n_agents))
+        self.n_joint_actions = 1
+        for actions in self.n_actions:
+            self.n_joint_actions*=actions
+        self.state_matrixs = np.reshape(state_matrixs, (n_states, -1, n_agents))
+        '''[state][joint_action][agent]'''
+        self.state_transition_matrix = np.reshape(state_transition_matrix, (n_states, self.n_joint_actions, n_states))
+        ''' [state][joint_action][next_state]'''
+        self.initial_state = initial_state
+        self.terminal_state = terminal_state
+        self.state = self.reset()
+
+    def reset(self, start_from=None):
+        if self.initial_state is None:
+            if start_from is None:
+                return 0
+            else:
+                return random.randint(0, self.n_states-1)
+        return self.initial_state()
+    
+    def state_matrix(self, state=None):
+        if state is None:
+            return self.state_matrixs[self.state]
+        return self.state_matrixs[state]
+    
+    def joint_reward(self, joint_action: np.ndarray | tuple, state=None):
+        index = self.flatten_joint_action(joint_action)
+        if state is None:
+            return self.state_matrixs[self.state][index]
+        return self.state_matrixs[state][index]
+        
+    def step(self, joint_action:np.ndarray|tuple):
+        joint_reward = self.joint_reward(joint_action)
+        index = self.flatten_joint_action(joint_action)
+        next_state = np.random.choice([i for i in range(self.n_states)], p=self.state_transition_matrix[self.state, index])
+        self.state = next_state
+        if self.terminal_state is not None:
+            is_terminated = next_state in self.terminal_state
+        return next_state, joint_reward, is_terminated
+
+
+# if __name__ == "__main__":
+#     s1 = np.array(
+#         [   [1, -1],[0, 0],
+#             [2, -2],[-1, 1]]
+#     )
+#     s2 = np.array(
+#         [   [3, -3],[0, 0],
+#             [-2, 2],[-3, 3]]
+#     )
+#     s3 = np.array(
+#         [   [0, 0],[0, 0],
+#             [1, -1],[-1, 1]]
+#     )
+#     st = np.array([
+#             [[1.0, 0.0, 0.0], [1.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.5, 0.5, 0.0]],
+#             [[0.0, 1.0, 0.0], [0.8, 0.0, 0.2], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]],
+#             [[0.0, 0.0, 1.0], [0.0, 0.0, 1.0], [1.0, 0.0, 0.0], [0.0, 0.0, 1.0]]
+#         ], ndmin=3)
+#     joint_action = np.array([1, 1])
+#     test = StochasticGame(
+#         n_agents=2,
+#         n_states=3,
+#         n_actions=2,
+#         state_matrixs=np.array([s1, s2, s3]),
+#         state_transition_matrix=st,
+#     )
+#     print(test.reward(joint_action=joint_action))
+#     test.step(joint_action=joint_action)
